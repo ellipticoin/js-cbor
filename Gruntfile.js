753bd11f0266ca7f48589544a62decc094e1cead
Mason Fischer <mason@kissr.co>
2020-12-28 13:06:31 -0500

    Install and run prettier

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,19 @@
-var path = require('path');
-var fs = require('fs');
+var path = require("path");
+var fs = require("fs");
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+  grunt.loadNpmTasks("grunt-mocha-test");
 
-	grunt.loadNpmTasks('grunt-mocha-test');
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    mochaTest: {
+      test: {
+        src: ["test/**/*.js"],
+      },
+    },
+  });
 
-	// Project configuration.
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		mochaTest: {
-			test: {
-				src: ['test/**/*.js']
-			}
-		}
-	});
-
-	// Default task(s).
-	grunt.registerTask('default', 'mochaTest');
+  // Default task(s).
+  grunt.registerTask("default", "mochaTest");
 };
